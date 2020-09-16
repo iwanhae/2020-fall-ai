9e5378bedc28392df904e3abe8753a68606f6e3c
iwanhae <iwanhae@gmail.com>
2020-09-16 06:00:52 +0000

    1주차

--- main.cpp
+++ main.cpp
@@ -7,23 +7,31 @@ struct aim {
     vector<double> input;
     double output;
 };
+
+//AND 입력데이터와 출력데이터
 vector<aim> data = {
     {{0, 0}, 0},
     {{1, 0}, 0},
     {{0, 1}, 0},
     {{1, 1}, 1}
 };
+
+//-1.0 ~ 1.0 사이의 임의의 실수 구하는 함수
 double getRandNum() {
     return ((rand() % 200) / 100.0) - 1.0;
 }
 int main()
 {
     srand((unsigned int)time(0));
+
+    //단일 노드
     Node singleNode;
+    //아주 단순한 binary Activation 함수
     auto f = [](double x) -> double { return 0 < x ? 1.0 : 0.0; };
 
     bool pass = false;
     while (!pass) {
+        //Random 하게 초기화
         singleNode.setBias(getRandNum());
         singleNode.setWeight(vector<double>{getRandNum(), getRandNum()});
         singleNode.setActivation(f);
@@ -33,10 +41,13 @@ int main()
         for(auto set : data) {          
             double in = singleNode.forward(set.input);
             double out = set.output;
-            cout << in << "\t" << out << "\t" << (in == out ? "GOOD" : "WRONG") << endl;            
+            cout << in << "\t" << out << "\t" << (in == out ? "GOOD" : "WRONG") << endl;
+
+            //하나로도 성공 못한 케이스가 있으면 다시 루프
             if(in != out) pass = false;
-        }        
+        }
     }
+    //노드정보 출력
     singleNode.print();
     return 0;
 }
